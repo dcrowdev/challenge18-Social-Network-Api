2721e5e1fd48bb0688e1819628203e48607f6a13
Dylan Crowley <dcrowdev1025@gmail.com>
2022-11-24 14:17:14 -0500

    initial reaction schema structure

---
+++ models/Reaction.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+
+        },
+        reactionBody: {
+
+        },
+        username: {
+
+        },
+        createdAt: {
+            
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+module.exports = reactionSchema;
