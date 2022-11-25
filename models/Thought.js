ba99c81d8df18aaf7fe1f6c8a02908de11b11817
Dylan Crowley <dcrowdev1025@gmail.com>
2022-11-25 06:32:54 -0500

    updated Thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,17 +1,25 @@
 const { Schema, model } = require('mongoose');
 const reactionSchema = require('./Reaction');
-const dateFormat = require('../utils/dateFormat');
+const { dateFormat } = require('../utils/dateFormat');
 
 const thoughtSchema = new Schema(
     {
         thoughtText: {
-
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
         },
         createdAt: {
-
+            type: Date,
+            default: Date.now(),
+            get: (date) => {
+                return dateFormat(date);
+            }
         },
         username: {
-
+            type: String,
+            required: true
         },
         reactions: [reactionSchema] // reference activity 17 and 18
     },
@@ -24,6 +32,9 @@ const thoughtSchema = new Schema(
 );
 
 // virtual for reaction count
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+  });
 
 const Thought = model('Thought', thoughtSchema);
 
